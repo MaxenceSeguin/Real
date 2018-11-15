7b012ad2c672a460be8923a140fa238deef0727c
Max <maxence.seguin.42@gmail.com>
2018-11-15 14:54:58 +0000

    Added documentation. Added features : - camera following the hero (hero in the middle of the screen) - camera not showing where there is no map

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -18,9 +18,7 @@ import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
-/**
- * Created by julienvillegas on 17/01/2017.
- */
+
 public class GameScreen implements Screen, InputProcessor {
 
     private OrthographicCamera camera;
@@ -31,18 +29,21 @@ public class GameScreen implements Screen, InputProcessor {
     private Sprite heroSprite;
 
 
-    private GameScreen(Game aGame) {
+    public GameScreen(Game aGame) {
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false,w,h);
         camera.update();
+
         tiledMap = new TmxMapLoader().load("map1.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
         Gdx.input.setInputProcessor(this);
 
         sb = new SpriteBatch();
+
         hero = new Hero("hero1.png", 0, 0);
         heroSprite = hero.getSprite();
     }
@@ -50,20 +51,37 @@ public class GameScreen implements Screen, InputProcessor {
     @Override
     public void show() {}
 
+    /**
+     *  This methods renders the graphics and keep them updated.
+     */
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         camera.update();
+
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
+
         sb.setProjectionMatrix(camera.combined);
         sb.begin();
         heroSprite.draw(sb);
+        heroMovement();
         sb.end();
+    }
+
+    /**
+     * This methods checks if the hero has to move.
+     * If so it also checks if the hero will enter in collision with any object
+     * in the COLLISION layer.
+     */
+    private void heroMovement(){
         if (hero.isMoving() && !isCollision(hero.getDx(), hero.getDy())){
             heroSprite.setPosition(heroSprite.getX()+hero.getDx(), heroSprite.getY()+hero.getDy());
+            camera.position.x = Math.max(Math.min(heroSprite.getX(), 1024 - camera.viewportWidth/2), camera.viewportWidth/2);
+            camera.position.y = Math.max(Math.min(heroSprite.getY(), 1024 - camera.viewportHeight/2), camera.viewportHeight/2);
         }
     }
 
@@ -92,27 +110,44 @@ public class GameScreen implements Screen, InputProcessor {
     public void dispose() {
     }
 
-
+    /**
+     *  This methods checks if the next move implies a collision between the hero and any
+     *  'rectangle' object from the 'COLLISION' layer.
+     *  Idea from https://stackoverflow.com/questions/20063281/libgdx-collision-detection-with-tiledmap
+     * @param dx direction of the hero on the abscissa
+     * @param dy direction of the hero on the ordinate
+     * @return false, no collision detected; true otherwise
+     */
     private boolean isCollision(int dx, int dy){
-        int objectLayerId = 1;
+
+        int objectLayerId = 1; /* The id of the COLLISION layer is 1 in our tilemap */
         MapLayer collisionObjectLayer = tiledMap.getLayers().get(objectLayerId);
         MapObjects objects = collisionObjectLayer.getObjects();
+
         Rectangle heroPos = heroSprite.getBoundingRectangle();
         heroPos.setX(heroPos.getX()+dx);
         heroPos.setY(heroPos.getY()+dy);
 
-        // there are several other types, Rectangle is probably the most common one
+        /*
+        *  We only treat the rectangle form the COLLISION layer, need improvement if other collision
+        *  shapes are added.
+        */
         for (RectangleMapObject rectangleObject : objects.getByType(RectangleMapObject.class)) {
 
             Rectangle rectangle = rectangleObject.getRectangle();
             if (Intersector.overlaps(rectangle, heroPos)) {
-                // collision happened
+                /* collision happened */
                 return true;
             }
         }
+
         return  false;
     }
 
+    /**
+     * Called when a key is pressed.
+     * @param keycode the key pressed
+     */
     @Override public boolean keyDown(int keycode) {
         if (keycode == Input.Keys.LEFT) {
             hero.setDx(-2);
@@ -129,6 +164,10 @@ public class GameScreen implements Screen, InputProcessor {
         return false;
     }
 
+    /**
+     * Called when a key is released.
+     * @param keycode the key released
+     */
     @Override public boolean keyUp(int keycode) {
         if (keycode == Input.Keys.LEFT || keycode == Input.Keys.RIGHT) {
             hero.setDx(0);
@@ -136,7 +175,6 @@ public class GameScreen implements Screen, InputProcessor {
         if (keycode == Input.Keys.UP || keycode == Input.Keys.DOWN) {
             hero.setDy(0);
         }
-        //camera.translate(0,-32);
         if(keycode == Input.Keys.NUM_1)
             tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
         if(keycode == Input.Keys.NUM_2)
@@ -144,11 +182,18 @@ public class GameScreen implements Screen, InputProcessor {
         return false;
     }
 
+    /**
+     * Called when a key is typed.
+     * @param character the character pressed
+     */
     @Override public boolean keyTyped(char character) {
 
         return false;
     }
 
+    /**
+     * Called when a mouse button is pressed.
+     */
     @Override public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         Vector3 clickCoordinates = new Vector3(screenX,screenY,0);
         Vector3 position = camera.unproject(clickCoordinates);
@@ -156,18 +201,29 @@ public class GameScreen implements Screen, InputProcessor {
         return true;
     }
 
+    /**
+     * Called when a mouse button is released.
+     */
     @Override public boolean touchUp(int screenX, int screenY, int pointer, int button) {
         return false;
     }
-
+    /**
+     * Called when the mouse is dragged.
+     */
     @Override public boolean touchDragged(int screenX, int screenY, int pointer) {
         return false;
     }
 
+    /**
+     * Called when the mouse is moved.
+     */
     @Override public boolean mouseMoved(int screenX, int screenY) {
         return false;
     }
 
+    /**
+     * Called when the mouse scroller is used.
+     */
     @Override public boolean scrolled(int amount) {
         return false;
     }
